265f7b03bf5c76b90f9f6cfcdaf02967cdf02dae
Stanislav Bezsmertnyi <bezst29@gmail.com>
2026-01-21 21:59:40 +0200

    Changed producer configuration view from UserControl to ReactiveUserControl

--- KafkaLoad.Desktop/Views/ProducerConfigurationView.axaml.cs
+++ KafkaLoad.Desktop/Views/ProducerConfigurationView.axaml.cs
@@ -1,13 +1,22 @@
-using Avalonia;
-using Avalonia.Controls;
+using KafkaLoad.Desktop.ViewModels;
+using ReactiveUI;
+using ReactiveUI.Avalonia;
+using ReactiveUI.Validation.Extensions;
 using Avalonia.Markup.Xaml;
+using Avalonia.Controls;
+using System.Reactive.Disposables.Fluent;
 
 namespace KafkaLoad.Desktop.Views;
 
-public partial class ProducerConfigurationView : UserControl
+public partial class ProducerConfigurationView : ReactiveUserControl<ProducerConfigurationViewModel>
 {
     public ProducerConfigurationView()
     {
         InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
     }
 }
