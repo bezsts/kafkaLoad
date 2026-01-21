8541b8c43331544db66efa574bbec13cade27856
Stanislav Bezsmertnyi <bezst29@gmail.com>
2026-01-21 14:09:08 +0200

    Implemented user control for producer configuration saving

---
+++ KafkaLoad.Desktop/Views/ProducerConfigurationView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace KafkaLoad.Desktop.Views;
+
+public partial class ProducerConfigurationView : UserControl
+{
+    public ProducerConfigurationView()
+    {
+        InitializeComponent();
+    }
+}
