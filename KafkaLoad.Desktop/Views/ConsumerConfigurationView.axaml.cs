f9effdf70c022f0ab1e82e2fd0709d677002298a
Stanislav Bezsmertnyi <bezst29@gmail.com>
2026-01-21 16:07:21 +0200

    Implemented ConsumerConfigurationView

---
+++ KafkaLoad.Desktop/Views/ConsumerConfigurationView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace KafkaLoad.Desktop.Views;
+
+public partial class ConsumerConfigurationView : UserControl
+{
+    public ConsumerConfigurationView()
+    {
+        InitializeComponent();
+    }
+}
