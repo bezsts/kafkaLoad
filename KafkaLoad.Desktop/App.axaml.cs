cc0528c99f3ddbd7760bf99798cf5fe0c83f70ae
bezsts <bezst29@gmail.com>
2026-02-12 23:28:42 +0200

    Optimize app.cs

--- KafkaLoad.Desktop/App.axaml.cs
+++ KafkaLoad.Desktop/App.axaml.cs
@@ -21,72 +21,93 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        var jsonConfigManager = new JsonConfigManager();
-        Locator.CurrentMutable.RegisterConstant(jsonConfigManager, typeof(IFileManager));
-
-        Locator.CurrentMutable.RegisterConstant(
-            new JsonConfigRepository<CustomProducerConfig>(jsonConfigManager, "Producers"),
-            typeof(IConfigRepository<CustomProducerConfig>));
-
-        Locator.CurrentMutable.RegisterConstant(
-            new JsonConfigRepository<CustomConsumerConfig>(jsonConfigManager, "Consumers"),
-            typeof(IConfigRepository<CustomConsumerConfig>));
-
-        Locator.CurrentMutable.RegisterConstant(
-            new JsonConfigRepository<TestScenario>(jsonConfigManager, "TestScenarios"),
-            typeof(IConfigRepository<TestScenario>));
-
-        Locator.CurrentMutable.RegisterConstant(
-            new KafkaClientFactory(), 
-            typeof(IKafkaClientFactory));
-
-        Locator.CurrentMutable.RegisterConstant(new MetricsService(), typeof(IMetricsService));
-
-        Locator.CurrentMutable.RegisterLazySingleton(() => 
-            new TestRunnerService(
-                Locator.Current.GetService<IKafkaClientFactory>()!,
-                Locator.Current.GetService<IMetricsService>()!
-            ), 
-            typeof(ITestRunnerService));
-
-        Locator.CurrentMutable.Register(() => 
-            new ProducerConfigView(), typeof(IViewFor<ProducerConfigViewModel>));
-        Locator.CurrentMutable.Register(() => 
-            new ConsumerConfigView(), typeof(IViewFor<ConsumerConfigViewModel>));
-        Locator.CurrentMutable.Register(() => 
-            new ClientsConfigView(), typeof(IViewFor<ClientsConfigViewModel>));
-        Locator.CurrentMutable.Register(() => 
-            new TestScenarioEditorView(), typeof(IViewFor<TestScenarioEditorViewModel>));
-        Locator.CurrentMutable.Register(() =>
-            new TestScenariosView(), typeof(IViewFor<TestScenariosViewModel>));
-        Locator.CurrentMutable.Register(() => 
-            new TestRunnerView(), typeof(IViewFor<TestRunnerViewModel>));
-
-        Locator.CurrentMutable.RegisterConstant(new KafkaTopicService(), typeof(IKafkaTopicService));
+        RegisterDependencies();
 
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            var producerConfigRepository = Locator.Current.GetService<IConfigRepository<CustomProducerConfig>>()!;
-            var consumerConfigRepository = Locator.Current.GetService<IConfigRepository<CustomConsumerConfig>>()!;
-            var testScenarioRepository = Locator.Current.GetService<IConfigRepository<TestScenario>>()!;
-            //var kafkaFactory = Locator.Current.GetService<IKafkaClientFactory>()!;
-            var testRunnerService = Locator.Current.GetService<ITestRunnerService>()!;
-            var metricsService = Locator.Current.GetService<IMetricsService>()!;
-            var kafkaTopicService = Locator.Current.GetService<IKafkaTopicService>()!;
-
-            desktop.MainWindow = new MainWindow
-            {
-                DataContext = new MainViewModel(
-                    producerConfigRepository, 
-                    consumerConfigRepository,
-                    testScenarioRepository, 
-                    //kafkaFactory,
-                    testRunnerService,
-                    metricsService,
-                    kafkaTopicService)
-            };
+            desktop.MainWindow = CreateMainWindow();
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    /// <summary>
+    /// Orchestrates the registration of all application components.
+    /// </summary>
+    private void RegisterDependencies()
+    {
+        RegisterAppServices();
+
+        RegisterRepositories();
+
+        RegisterViews();
+    }
+
+    private void RegisterAppServices()
+    {
+        // Core services
+        Locator.CurrentMutable.RegisterConstant(new JsonConfigManager(), typeof(IFileManager));
+        Locator.CurrentMutable.RegisterConstant(new MetricsService(), typeof(IMetricsService));
+        Locator.CurrentMutable.RegisterConstant(new KafkaClientFactory(), typeof(IKafkaClientFactory));
+        Locator.CurrentMutable.RegisterConstant(new KafkaTopicService(), typeof(IKafkaTopicService));
+
+        Locator.CurrentMutable.RegisterLazySingleton(() =>
+            new TestRunnerService(
+                Locator.Current.GetService<IKafkaClientFactory>()!,
+                Locator.Current.GetService<IMetricsService>()!
+            ),
+            typeof(ITestRunnerService));
+    }
+
+    private void RegisterRepositories()
+    {
+        var fileManager = Locator.Current.GetService<IFileManager>();
+
+        // Producers
+        Locator.CurrentMutable.RegisterConstant(
+            new JsonConfigRepository<CustomProducerConfig>(fileManager!, "Producers"),
+            typeof(IConfigRepository<CustomProducerConfig>));
+
+        // Consumers
+        Locator.CurrentMutable.RegisterConstant(
+            new JsonConfigRepository<CustomConsumerConfig>(fileManager!, "Consumers"),
+            typeof(IConfigRepository<CustomConsumerConfig>));
+
+        // Scenarios
+        Locator.CurrentMutable.RegisterConstant(
+            new JsonConfigRepository<TestScenario>(fileManager!, "TestScenarios"),
+            typeof(IConfigRepository<TestScenario>));
+    }
+
+    private void RegisterViews()
+    {
+        Locator.CurrentMutable.Register(() => new ProducerConfigView(), typeof(IViewFor<ProducerConfigViewModel>));
+        Locator.CurrentMutable.Register(() => new ConsumerConfigView(), typeof(IViewFor<ConsumerConfigViewModel>));
+        Locator.CurrentMutable.Register(() => new ClientsConfigView(), typeof(IViewFor<ClientsConfigViewModel>));
+
+        Locator.CurrentMutable.Register(() => new TestScenarioEditorView(), typeof(IViewFor<TestScenarioEditorViewModel>));
+        Locator.CurrentMutable.Register(() => new TestScenariosView(), typeof(IViewFor<TestScenariosViewModel>));
+
+        Locator.CurrentMutable.Register(() => new TestRunnerView(), typeof(IViewFor<TestRunnerViewModel>));
+    }
+
+    /// <summary>
+    /// Resolves dependencies and creates the Main Window.
+    /// </summary>
+    private MainWindow CreateMainWindow()
+    {
+        var resolver = Locator.Current;
+
+        return new MainWindow
+        {
+            DataContext = new MainViewModel(
+                resolver.GetService<IConfigRepository<CustomProducerConfig>>()!,
+                resolver.GetService<IConfigRepository<CustomConsumerConfig>>()!,
+                resolver.GetService<IConfigRepository<TestScenario>>()!,
+                resolver.GetService<ITestRunnerService>()!,
+                resolver.GetService<IMetricsService>()!,
+                resolver.GetService<IKafkaTopicService>()!
+            )
+        };
     }
 }
