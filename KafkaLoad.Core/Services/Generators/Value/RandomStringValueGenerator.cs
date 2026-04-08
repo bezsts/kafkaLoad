62cfeac8c13cbc8ef5f1ce8f4939f83e7b9db7af
bezsts <bezst29@gmail.com>
2026-04-08 22:22:50 +0300

    Optimized algorithm of generating random string

--- KafkaLoad.Core/Services/Generators/Value/RandomStringValueGenerator.cs
+++ KafkaLoad.Core/Services/Generators/Value/RandomStringValueGenerator.cs
@@ -6,28 +6,26 @@ namespace KafkaLoad.Core.Services.Generators.Value
 {
     public class RandomStringValueGenerator : IDataGenerator
     {
-        private readonly int _size;
-        private readonly Random _random = new();
         private const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        private readonly int _size;
+        private readonly char[] _pool;
+        private readonly int _maxOffset;
 
         public RandomStringValueGenerator(int size)
         {
             _size = size;
+            int poolSize = Math.Max(size * 16, 65536);
+            _pool = new char[poolSize];
+            for (int i = 0; i < poolSize; i++)
+                _pool[i] = Chars[Random.Shared.Next(Chars.Length)];
+            _maxOffset = poolSize - size;
         }
 
         public byte[] Next()
         {
-            // Note: Generating random strings char-by-char is CPU intensive.
-            // Optimally, for load testing, we pre-generate a large buffer and take slices,
-            // but for simplicity, here is the direct implementation.
-            // TODO: check another algorithm
-
-            var stringChars = new char[_size];
-            for (int i = 0; i < _size; i++)
-            {
-                stringChars[i] = Chars[_random.Next(Chars.Length)];
-            }
-            return Encoding.UTF8.GetBytes(new string(stringChars));
+            int offset = Random.Shared.Next(_maxOffset);
+            return Encoding.UTF8.GetBytes(_pool, offset, _size);
         }
     }
 }
